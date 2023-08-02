bb83629925f5a8ea4f6e15ce26c53b909e896359
unknown <sayedmohamed123.sm74@gmail.com>
2023-08-02 11:12:36 +0300

    last

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -10,7 +10,7 @@ const MainPage = ({ api }) => {
   const [latestProducts, setLastestProducts] = useState([]);
   const [clothes, setClothes] = useState([]);
   const [elecrtonics, setElecrtonics] = useState([]);
-
+  const [mobiles, setMobiles] = useState([]);
   // Fetch Latest Products
   useEffect(() => {
     const fetchLatestProducts = async () => {
@@ -30,6 +30,16 @@ const MainPage = ({ api }) => {
     };
     fetchClothes();
   }, [clothes, api]);
+
+  // Fetch Mobiles
+  useEffect(() => {
+    const fetchClothes = async () => {
+      await Axios.get(`${api}api/products/1/mobiles/default`)
+        .then((res) => setClothes(res.data))
+        .catch((err) => console.log(err));
+    };
+    fetchClothes();
+  }, [mobiles, api]);
 
   // Fetch Electronics
   useEffect(() => {
@@ -70,6 +80,23 @@ const MainPage = ({ api }) => {
           <h1 className="mb-5 text-3xl font-medium text-heading">Clothes</h1>
           <div className="flex flex-col md:flex-row justify-between gap-6">
             {clothes.slice(0, 5).map((product) => (
+              <Product product={product} key={product._id} />
+            ))}
+          </div>
+          <Link to="/store">
+            <button className="px-6 py-2.5 rounded-3xl text-white bg-gray-700  mt-5 hover:bg-gray-500 hover:text-black ease-in duration-100">
+              VIEW MORE
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      {/* mobiles */}
+      <section className="my-12">
+        <div className="container mx-auto px-6 py-5 text-center rounded-3xl shadow-2xl bg-white">
+          <h1 className="mb-5 text-3xl font-medium text-heading">Clothes</h1>
+          <div className="flex flex-col md:flex-row justify-between gap-6">
+            {mobiles.slice(0, 5).map((product) => (
               <Product product={product} key={product._id} />
             ))}
           </div>
