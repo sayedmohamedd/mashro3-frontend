d8c508a25db20789dde3ea2ae2c0c637ccdf5c04
sayedmohamed <sayedmo.web.developer@gmail.com>
2026-01-19 20:27:29 +0200

    update env

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,5 @@
 // const url = 'https://mashro3-backend.onrender.com';
-const url = 'http://localhost:3002';
+const url= "https://mashro3-backend.vercel.app"
+// const url = 'http://localhost:3002';
 
 export default url;
